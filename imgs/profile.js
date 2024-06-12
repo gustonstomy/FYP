ccad9b65c36b43354a15a3655c31f05dc6b0140e
gustonstomy <gustomstomy@gmail.com>
2024-06-12 22:27:01 +0100

    yes

---
+++ imgs/profile.js
@@ -0,0 +1,19 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const profilePicture = document.querySelector(".profile-picture");
+//     const fileInput = document.getElementById("fileInput");
+
+//     profilePicture.addEventListener("click", function () {
+//         fileInput.click();
+//     });
+
+//     fileInput.addEventListener("change", function () {
+//         const file = this.files[0];
+//         if (file) {
+//             const reader = new FileReader();
+//             reader.onload = function (e) {
+//                 profilePicture.querySelector("img").src = e.target.result;
+//             };
+//             reader.readAsDataURL(file);
+//         }
+//     });
+// });
